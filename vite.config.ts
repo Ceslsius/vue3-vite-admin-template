e5394c18170d477600e3f21bfc2eb5fe9e065319
Yi Yunwan <yyw9603@163.com>
2021-03-23 14:38:44 +0800

    feat:修改打包错误

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
  * @Author: Yi Yunwan
  * @Date: 2021-03-10 11:28:58
  * @LastEditors: Yi Yunwan
- * @LastEditTime: 2021-03-23 14:31:09
+ * @LastEditTime: 2021-03-23 14:37:53
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -11,7 +11,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: './',
+  base: './',
   css: {
     preprocessorOptions: {
       scss: {
