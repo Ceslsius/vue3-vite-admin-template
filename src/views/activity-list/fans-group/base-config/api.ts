0c3a38d2d1154e1990d8182f21e361f01c21f7ec
Yi Yunwan <yyw9603@163.com>
2021-04-07 15:00:04 +0800

    暂存

--- src/views/activity-list/fans-group/base-config/api.ts
+++ src/views/activity-list/fans-group/base-config/api.ts
@@ -3,11 +3,11 @@
  * @Author: Yi Yunwan
  * @Date: 2021-04-06 17:31:09
  * @LastEditors: Yi Yunwan
- * @LastEditTime: 2021-04-07 09:36:34
+ * @LastEditTime: 2021-04-07 14:44:10
  */
 
 import { service } from '@/utils/http'
 
-export function setBaseConfig(data: BaseConfigData) {
+export function setBaseConfig(data: BaseConfigData): Res {
   return service.post('/backend/base/config', data)
 }
--- src/views/activity-list/fans-group/base-config/componens/PowderRising.vue
+++ src/views/activity-list/fans-group/base-config/componens/PowderRising.vue
@@ -3,98 +3,190 @@
  * @Author: Yi Yunwan
  * @Date: 2021-04-06 17:26:25
  * @LastEditors: Yi Yunwan
- * @LastEditTime: 2021-04-07 12:24:00
+ * @LastEditTime: 2021-04-07 14:58:12
 -->
 <template>
-  <el-form :model="list" size="mini" label-width="90px">
-    <el-row v-for="(item, index) in list" :key="index">
-      <el-col>
+  <el-form :model="list" ref="formRef" size="mini" label-width="100px">
+    <el-row
+      v-for="(item, index) in list"
+      :key="index"
+      type="flex"
+      align="bottom"
+      class="border p-10 radius mb-15"
+    >
+      <el-col class="text-bold pb-10 mb-15 border-bottom">
+        涨粉配置{{ index + 1 }}：
+        <span class="form-item-tips">
+          <i class="el-icon-info"></i>
+          头像框和热度奖励二选一，当选择热度值时，钻石数必填
+        </span>
+      </el-col>
+      <el-col :span="22">
         <el-row type="flex">
-          <el-col :span="8">
-            <el-form-item label="涨粉数量">
+          <el-col :span="9">
+            <el-form-item
+              label="涨粉数量"
+              :prop="`[${index}].num`"
+              :rules="{
+                required: true,
+                message: '请输入涨粉数量',
+              }"
+            >
               <el-input
                 v-model.number="item.num"
                 placeholder="请输入涨粉数量"
+                clearable
               ></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row type="flex">
-          <el-col :span="8">
-            <el-form-item label="赠送钻石数">
+          <el-col :span="9">
+            <el-form-item
+              label="赠送钻石数"
+              :prop="`[${index}].coin`"
+              :rules="{
+                required:
+                  item.hot_rec.value || item.hot_rec.url || item.hot_rec.time,
+                message: '请输入赠送钻石数',
+              }"
+            >
               <el-input
                 v-model.number="item.coin"
                 placeholder="请输入赠送钻石数"
+                clearable
               ></el-input>
             </el-form-item>
           </el-col>
         </el-row>
 
         <el-row type="flex">
-          <el-col :span="8">
-            <el-form-item label="热度值">
+          <el-col :span="9">
+            <el-form-item
+              label="热度值"
+              :prop="`[${index}].hot_rec.value`"
+              :rules="{
+                required:
+                  item.hot_rec.value || item.hot_rec.url || item.hot_rec.time,
+                message: '请输入热度值',
+              }"
+            >
               <el-input
                 v-model.number="item.hot_rec.value"
                 placeholder="请输入热度值"
+                clearable
               ></el-input>
             </el-form-item>
           </el-col>
-          <el-col :span="8">
-            <el-form-item label="时长">
+          <el-col :span="9">
+            <el-form-item
+              label="时长"
+              :prop="`[${index}].hot_rec.time`"
+              :rules="{
+                required:
+                  item.hot_rec.value || item.hot_rec.url || item.hot_rec.time,
+                message: '请输入时长',
+              }"
+            >
               <el-input
                 v-model.number="item.hot_rec.time"
                 placeholder="请输入时长"
+                clearable
               ></el-input>
             </el-form-item>
           </el-col>
-          <el-col :span="8">
-            <el-form-item label="奖励图片">
-              <el-input
-                v-model.number="item.hot_rec.url"
-                placeholder="请输入奖励图片"
-              ></el-input>
+          <el-col :span="6">
+            <el-form-item
+              label="奖励图片"
+              :prop="`[${index}].hot_rec.url`"
+              :rules="{
+                required:
+                  item.hot_rec.value || item.hot_rec.url || item.hot_rec.time,
+                message: '请上传奖励图片',
+              }"
+            >
+              <ImageUpload v-model:url="item.hot_rec.url"></ImageUpload>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row type="flex">
-          <el-col :span="8">
-            <el-form-item label="地址">
-              <el-input placeholder="请输入热度值"></el-input>
+          <el-col :span="9">
+            <el-form-item
+              label="头像框名称"
+              :prop="`[${index}].avatar.name`"
+              :rules="{
+                required:
+                  item.avatar.name || item.avatar.url || item.avatar.time,
+                message: '请输入头像框名称',
+              }"
+            >
+              <el-input
+                v-model="item.avatar.name"
+                placeholder="请输入头像框名称"
+                clearable
+              ></el-input>
             </el-form-item>
           </el-col>
-          <el-col :span="8">
-            <el-form-item label="名称">
-              <el-input placeholder="请输入头像框名称"></el-input>
+          <el-col :span="9">
+            <el-form-item
+              label="使用时长"
+              :prop="`[${index}].avatar.time`"
+              :rules="{
+                required:
+                  item.avatar.name || item.avatar.url || item.avatar.time,
+                message: '请输入使用时长',
+              }"
+            >
+              <el-input
+                v-model.number="item.avatar.time"
+                placeholder="请输入使用时长"
+                clearable
+              ></el-input>
             </el-form-item>
           </el-col>
-          <el-col :span="8">
-            <el-form-item label="时长">
-              <el-input placeholder="请输入时长"></el-input>
+          <el-col :span="6">
+            <el-form-item
+              label="头像框"
+              :prop="`[${index}].avatar.url`"
+              :rules="{
+                required:
+                  item.avatar.name || item.avatar.url || item.avatar.time,
+                message: '请上传头像框',
+              }"
+            >
+              <ImageUpload v-model:url="item.avatar.url"></ImageUpload>
             </el-form-item>
           </el-col>
         </el-row>
       </el-col>
-      <el-col class="mb-15">
-        <el-button type="danger" @click="delList(index)" v-if="list.length > 1">
-          删除
-        </el-button>
-        <el-button
-          type="primary"
-          @click="addList"
-          v-if="index === list.length - 1"
-        >
-          增加
-        </el-button>
+      <el-col :span="2">
+        <el-row>
+          <el-col v-if="list.length > 1">
+            <el-button type="danger" @click="delList(index)"> 删除 </el-button>
+          </el-col>
+          <el-col v-if="index === list.length - 1" class="mt-15">
+            <el-button type="primary" @click="addList"> 增加 </el-button>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
+    <el-button type="primary" @click="onSubmit">保存</el-button>
   </el-form>
 </template>
 
 <script lang="ts">
+import ImageUpload from '@/components/ImageUpload/ImageUpload.vue'
 import { useDynamicForm } from '@/use/useDynamicForm'
+import { useForm } from '@/use/useForm'
+import { ElMessage } from 'element-plus'
 import { defineComponent, reactive } from 'vue'
+import { setBaseConfig } from '../api'
+
 export default defineComponent({
   name: 'PowderRising',
+  components: {
+    ImageUpload,
+  },
   setup() {
     const { list, addList, delList } = useDynamicForm<FanMissionInfo>({
       hot_rec: {
@@ -106,10 +198,20 @@ export default defineComponent({
       },
     })
 
+    const { btnLoading, onSubmit, formRef } = useForm(async () => {
+      const { msg } = await setBaseConfig({
+        fan_mission: list,
+      })
+      ElMessage.success(msg)
+    })
+
     return {
       list,
       addList,
       delList,
+      btnLoading,
+      onSubmit,
+      formRef,
     }
   },
 })
