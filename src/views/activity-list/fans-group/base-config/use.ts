b478d907d26568ee85fabd5bca58e758881ae795
Yi Yunwan <yyw9603@163.com>
2021-04-15 09:45:56 +0800

    fix:解决缓存冲突

--- src/views/activity-list/fans-group/base-config/use.ts
+++ src/views/activity-list/fans-group/base-config/use.ts
@@ -3,7 +3,7 @@
  * @Author: Yi Yunwan
  * @Date: 2021-04-12 10:08:27
  * @LastEditors: Yi Yunwan
- * @LastEditTime: 2021-04-12 12:08:32
+ * @LastEditTime: 2021-04-15 09:45:41
  */
 import { adminStorage, jsonClone } from '@/utils'
 import { reactive } from 'vue'
@@ -14,14 +14,14 @@ export function useFansConfig(callBack: Function) {
   const fanMission = reactive<FanMissionInfo[]>(
     adminStorage.getItem('fanMission') || []
   )
-  const reward = reactive(adminStorage.getItem('anchorReward') || {})
+  const reward = reactive(adminStorage.getItem('fansAnchorReward') || {})
   getActivityRule().then(({ data }) => {
     const fanMissionList = JSON.parse(data.base || '{}').fan_mission || []
     const anchorReward = JSON.parse(data.anchor_reward || '{}') || {}
     Object.assign(fanMission, fanMissionList)
     Object.assign(reward, anchorReward)
     adminStorage.setItem('fanMission', fanMissionList)
-    adminStorage.setItem('anchorReward', anchorReward)
+    adminStorage.setItem('fansAnchorReward', anchorReward)
     callBack()
   })
 
